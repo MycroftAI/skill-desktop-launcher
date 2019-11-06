a869615d8eb181456b4473552539ac7354751e7a
Åke Forslund <ake.forslund@gmail.com>
2019-11-06 14:52:20 +0100

    Add mocking of webbrowser

---
+++ test/__init__.py
@@ -0,0 +1,22 @@
+from test.integrationtests.skills.skill_tester import SkillTest
+import mock
+
+
+SEARCH_BASE = 'http://www.google.com/search?&sourceid=navclient&btnI=I&q={}'
+
+
+@mock.patch('mycroft.skills.mycroft_skill.mycroft_skill.DeviceApi.send_email')
+def test_runner(skill, example, emitter, loader, m1):
+    s = [s for s in loader.skills if s and s.root_dir == skill][0]
+    with mock.patch('webbrowser.open') as m:
+        ret = SkillTest(skill, example, emitter).run(loader)
+        if example.endswith('find.puppies.json'):
+            m.assert_called_with(SEARCH_BASE.format('wikipedia%20puppies'))
+        elif example.endswith('find.puppies.json'):
+            m.assert_called_with(SEARCH_BASE.format('imgur'))
+        elif example.endswith('open.tumblr.json'):
+            m.assert_called_with(SEARCH_BASE.format('tumblr'))
+        elif example.endswith('search.for.kittens.json'):
+            m.assert_called_with(SEARCH_BASE.format('youtube%20for%20kittens'))
+
+    return ret
